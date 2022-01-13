00e47e349df4ceed0d508b3055a8b39e1d30be38
klas <klas@pop-os.localdomain>
2022-01-13 16:48:31 +0100

    add ssh init script

---
+++ init-ssh.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+if [ -z $1 ]
+then
+    echo "Username is required"
+    exit 1
+fi
+if [ -z $2 ]
+then
+    echo "Password is required"
+    exit 1
+fi
+apt-get update -y
+apt-get install -y openssh-server sudo
+mkdir /run/sshd
+PATH=/usr/sbin/:$PATH
+useradd -G sudo -m -s /bin/bash $1
+echo "$1:$2" | chpasswd
+/usr/sbin/sshd -D
